1732f45c2ad41d746ec276dc6d647a073b0caf37
QuantaEvo <139903190+QuantaEvo@users.noreply.github.com>
2023-10-28 10:51:50 +0530

    Update streamlit_app.py
    Changed Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit 
 
-streamlit.title('My Parent New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
 streamlit.text('Breakfast Menu')
-streamlit.text('Ots Meal')
-streamlit.text('Corn Soup')
-streamlit.text('Veg Upma')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
